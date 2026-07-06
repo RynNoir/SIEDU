95dfb9c3d97d6fa7328105adf06231b184ccb88e
RynNoir <reyhandwisaputra1234@gmail.com>
2026-07-06 23:45:56 +0700

    Fase 2 : Membuat model Course.php

---
+++ app/Models/Course.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\CourseFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable(['study_program_id', 'name', 'code', 'semester', 'credit_hours'])]
+class Course extends Model
+{
+    /** @use HasFactory<CourseFactory> */
+    use HasFactory;
+
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'semester' => 'integer',
+            'credit_hours' => 'integer',
+        ];
+    }
+
+    /**
+     * @return BelongsTo<StudyProgram, $this>
+     */
+    public function studyProgram(): BelongsTo
+    {
+        return $this->belongsTo(StudyProgram::class);
+    }
+
+    /**
+     * @return HasMany<CourseClassAssignment, $this>
+     */
+    public function courseClassAssignments(): HasMany
+    {
+        return $this->hasMany(CourseClassAssignment::class);
+    }
+}
